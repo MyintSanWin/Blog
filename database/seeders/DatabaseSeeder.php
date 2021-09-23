d5aefa04363d0d60478eaeb6dd08cbb0a7aa0a4f
MyintSanWin <jackmee24680@gmail.com>
2021-09-23 20:13:59 +0630

    Database seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +16,47 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+
+     {
+       User::truncate();
+       Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+      $personal=  Category::create([
+                'name' => 'Personal',
+                'slug' => 'personal'
+        ]);
+
+       $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+    ]);
+
+    $work =Category::create([
+        'name' => 'Work',
+        'slug' => 'work'
+]);
+Post::create([
+    'user_id' => $user->id,
+    'category_id' => $family->id,
+    'title' => 'My Family Post',
+    'slug' => 'my-first-post',
+    'excerpt' =>'<p>Lorem ipsum dolar sit amet.</p>',
+    'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fugit at ratione eos voluptas perspiciatis quaerat voluptatum nisi cumque dolorem nobis doloribus voluptatibus, optio ipsa beatae corrupti nesciunt vero similique.</p>'
+]);
+
+Post::create([
+    'user_id' => $user->id,
+    'category_id' => $work->id,
+    'title' => 'My Work Post',
+    'slug' => 'my-work-post',
+    'excerpt' =>'<p>Lorem ipsum dolar sit amet.</p>',
+    'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fugit at ratione eos voluptas perspiciatis quaerat voluptatum nisi cumque dolorem nobis doloribus voluptatibus, optio ipsa beatae corrupti nesciunt vero similique.</p>'
+]);
+
     }
+
 }
+
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
         {!!$post->title!!}
 
         <p>
-            <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+        By <a href="#">{{$post->user->name}}</a> in  <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
           </p>
          <div style="padding-top: 15px; ">
              {!! $post->body !!}
