a3fa9ddc79745d51cadbde1c27c7cb784bd4ad09
MyintSanWin <jackmee24680@gmail.com>
2021-09-28 15:05:39 +0630

    A small javascript dropdown detour

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,11 @@ use Symfony\Component\VarDumper\VarDumper;
 
 Route::get('/', function () {
         
-        return view('posts',[
-            'posts'=> Post::latest()->with()->get()
+   
+           return view('posts',[
+            'posts'=> Post::latest()->get(),
+            'categories' => Category::all()
+
         ]);
    
 });
@@ -40,7 +43,9 @@ Route::get('posts/{post:slug}',function(Post $post){
 
 Route::get('categories/{category:slug}', function (Category $category){
   return view('posts', [
-    'posts' => $category->posts
+    'posts' => $category->posts,
+    'currentCategory' => $category,
+    'categories' => Category::all()
   ]);
 
 });
@@ -49,7 +54,8 @@ Route::get('categories/{category:slug}', function (Category $category){
 Route::get('authors/{author:username}', function (User $author ){
   
   return view('posts', [
-    'posts' => $author->posts
+    'posts' => $author->posts,
+    'categories'=>Category::all()
   ]);
 
 });
