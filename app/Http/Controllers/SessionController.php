17eea60e2fda341acbe9d045ef77a76738d14ac3
MyintSanWin <jackmee24680@gmail.com>
2021-10-10 14:00:51 +0630

    Buil the log in page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,13 +4,40 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class SessionController extends Controller
 {
+
+    public function create(){
+        return view('sessions.create');
+    }
+
+    function store(){
+        //validate the request
+        $attributes =request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //attempt to authentication and log in the user
+        //based on the provided credentials
+        if(Auth::attempt($attributes)){
+            //session fixation
+            session()->regenerate();
+              //redirect with a success flash message
+              return redirect('/')->with('success', 'Welcome back!  Yor are logged in');
+        }
+        //auth failed
+       throw ValidationException::withMessages(['email' => 'Your provided credentals could not be verified.']);
+      
+      
+    }
     //
     function destroy(){
        Auth::logout();
 
-       return redirect('/')->with('success', 'Goodbye!');
+       return redirect('/login')->with('success', 'Goodbye!');
     }
 }
